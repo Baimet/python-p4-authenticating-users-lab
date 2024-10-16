af6ed7462d36b62287851907143dcb5fcc9931de
baimet <isaacbaimetyatich@gmail.com>
2024-10-16 08:07:26 +0300

    Authentication

--- server/app.py
+++ server/app.py
@@ -27,6 +27,22 @@ class ClearSession(Resource):
 
         return {}, 204
 
+class Login(Resource):
+    
+    def post(self):
+        username = request.json.get('username')
+
+        # Fetch the user from the database
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            # Set the user_id in the session
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'error': 'User not found'}, 404
+
+
 class IndexArticle(Resource):
     
     def get(self):
@@ -47,10 +63,34 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+
+
+class Logout(Resource):
+
+    def delete(self):
+        # Clear the session by removing user_id
+        session.pop('user_id', None)
+        return {}, 204
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
 
 
 if __name__ == '__main__':
